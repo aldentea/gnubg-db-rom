8ac55bf43c17eca7d7a6f4a9fb43f94de66dfff6
aldentea <aldentea-dev@mail.misobread.0am.jp>
2023-08-08 07:45:30 +0000

    Associate sessions and players.

--- root/test.rb
+++ root/test.rb
@@ -8,18 +8,41 @@ end
 db_filename = ARGV[0]
 
 rom = ROM.container(:sql, "sqlite://#{db_filename}") do |config|
-  config.relation(:session) do
+  config.relation(:player) do
     schema(infer: true)
     auto_struct true
+  end
+  config.relation(:session) do
+    schema(infer: true) do
+      associations do
+        belongs_to :player, as: :player0, relation: :player, foreign_key: :player_id0
+        belongs_to :player, as: :player1, relation: :player, foreign_key: :player_id1
+      end
+    end
+    auto_struct false # 無意味？
   end
 end
 
 module GnubgDatabase
 
-  class Session < ::ROM::Repository[:session]
+  class SessionRepo < ::ROM::Repository[:session]
+    def query(conditions)
+      session.where(conditions).to_a
+    end
+
+    def by_id(id)
+      session.combine(:player0, :player1).by_pk(id).one!
+    end
+
   end
 
 end
 
-session = GnubgDatabase::Session.new(rom)
+session_repo = GnubgDatabase::SessionRepo.new(rom)
+
+match = session_repo.by_id(4)
+p match.player0
+p match.player1
+
+#p session_repo.query(player_id0: 2)
 
